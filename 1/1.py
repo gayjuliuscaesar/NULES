12e24bfbec1334c697476ac8cee28714b66d8b7a
skiriks <koaccforwork@gmail.com>
2023-04-11 21:32:31 +0300

    First laba is done.

---
+++ 1/1.py
@@ -0,0 +1,16 @@
+class Hexagon:
+    def __init__(self, a=0):
+        self.a = a
+
+    def get_a(self):
+        return self.a
+
+    def set_a(self, a):
+        self.a = a
+
+    def area(self):
+        return (3 * (3 ** 0.5) * self.a ** 2) / 2
+
+    def print_info(self):
+        print("Шайба зі стороною довжиною ", self.a)
+        print("Площа шайби: ", self.area())
