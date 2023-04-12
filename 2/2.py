fec7e97f0b1bcb8079496ba6b4d27a90e550eabd
skiriks <koaccforwork@gmail.com>
2023-04-12 18:42:01 +0300

    2 laba is done.

--- 2/2.py
+++ 2/2.py
@@ -1,6 +1,29 @@
-def main():
-    pass
+class Cross:
+    def __init__(self, a=0, b=0):
+        self.a = a
+        self.b = b
+
+    def get_a(self):
+        return self.a
+
+    def set_a(self, a):
+        self.a = a
+
+    def get_b(self):
+        return self.b
+
+    def set_b(self, b):
+        self.b = b
+
+    def area(self):
+        return self.a * self.b
+
+    def print_sqrt(self):
+        print(f"Cross with sides a = {self.a}, b = {self.b} has area={self.area()}")
 
 
-if __name__ == '__main__':
-    main()
+def frd(cross1, cross2):
+    a = cross1.get_a() + cross2.get_a()
+    b = cross1.get_b() + cross2.get_b()
+    frd_cross = Cross(a, b)
+    return frd_cross
