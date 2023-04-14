3c25460a0cc8e81c1bd6d89a5af1962eb9700bd1
skiriks <koaccforwork@gmail.com>
2023-04-14 13:45:33 +0300

    2 laba is done.

--- 3/3.py
+++ 3/3.py
@@ -1,5 +1,37 @@
+import locale
+
+locale.setlocale(locale.LC_ALL, 'ru_RU.UTF-8')
+
+
+class Student:
+
+    def __init__(self, name, math, ph, info):
+        self.name = name
+        self.math = math
+        self.ph = ph
+        self.info = info
+
+    def get_ph(self):
+        print(self.name + ' - ' + self.ph)
+
+
 def main():
-    pass
+    grade = input('Введіть бал по фізиці, вибірку з якого бажали б отримати: ')
+
+    with open('data.txt', 'r', encoding='utf-8') as data:
+        stud_list = []
+        for line in data:
+            stud = Student(*line.split())
+            if line.split()[-2] == grade:
+                stud_list.append(stud)
+
+    sorted_list = sorted(stud_list, key=lambda x: x.name)
+
+    for stud in sorted_list:
+        stud.get_ph()
+
+    print('-------------------')
+    print(f'Загальна кількість учнів з оцінкою з фізики у {grade} балів: {len(stud_list)}')
 
 
 if __name__ == '__main__':
